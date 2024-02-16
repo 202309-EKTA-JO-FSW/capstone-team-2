0226296ddf7a705846f35fa2d0fadb84084d7f24
Farah Alsoqi <Farahmanna97@gmail.com>
2024-02-16 23:32:48 +0300

    use prettier for coordination

--- server/server/models/User.js
+++ server/server/models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-
   email: {
     type: String,
     required: true,
@@ -32,29 +31,27 @@ const userSchema = new mongoose.Schema({
     type: Number,
     required: true,
     validate: {
-        validator: function(v) {
-            // This regex allows numbers, spaces, parentheses, and hyphens
-            return /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(v);
-        },
-        message: props => `${props.value} is not a valid phone number format`
-    }
+      validator: function (v) {
+        // This regex allows numbers, spaces, parentheses, and hyphens
+        return /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number format`,
+    },
   },
 
   preferences: {
     type: String,
   },
 
-  profilePicture:{
+  profilePicture: {
     type: String,
   },
 
   role: {
     type: String,
-    enum: ['user', 'admin'],
-    default: 'user'
-}
-
+    enum: ["user", "admin"],
+    default: "user",
+  },
 });
-
 
 module.exports = mongoose.model("userSchema", userSchema);
