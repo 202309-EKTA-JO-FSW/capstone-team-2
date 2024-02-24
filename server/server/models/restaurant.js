d00496fe576be15eb3528357a0bd9ac59e58b1f5
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-02-25 00:32:00 +0300

    modify restaurant  models

--- server/server/models/restaurant.js
+++ server/server/models/restaurant.js
@@ -1,65 +1,11 @@
-/* last one 
-const mongoose = require("mongoose");
-
-const restaurantSchema = new mongoose.Schema({
-  dishID: { type: mongoose.Schema.Types.ObjectId, required: true },
-  name: { type: String, required: true },
-  restaurantLocation: { type: String, required: true },
-  imageURL: { type: String, required: true },
-  description: { type: String, required: true },
-  userLocation: { type: String, required: true },
-});
-
-module.exports = mongoose.model("restaurantSchema", restaurantSchema);
-*/
-
-/*
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const restaurantSchema = new Schema({
-  
-  name: {
-    type: String,
-    required: true,
-  },
-
-  restaurantLocation: {
-    type: String,
-    required: true,
-  },
-
-  imageURL: { 
-    type: String, 
-    required: true },
-
-  description: { 
-    type: String, 
-    required: true },
-
-  dishes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Dish",
-    },
-  ],
-
-});
-
-module.exports = mongoose.model("Restaurant", restaurantSchema);
-
-*/
-
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-
-// review embedded 
+// review embedded
 
 const reviewSchema = new mongoose.Schema({
   dishID: {
     type: mongoose.Schema.Types.ObjectId,
-    // ref: "dishSchema",
     ref: "DishModel",
     required: true,
   },
@@ -84,7 +30,6 @@ const reviewSchema = new mongoose.Schema({
   },
 });
 
-////
 const restaurantSchema = new Schema({
   name: {
     type: String,
@@ -112,11 +57,7 @@ const restaurantSchema = new Schema({
       ref: "DishModel",
     },
   ],
-  reviewSchema : [reviewSchema],
-  // review: {
-  //   type: [String],
-  //   required: true,
-  // },
+  reviews: [reviewSchema],
 });
 
 module.exports = mongoose.model("RestaurantModel", restaurantSchema);
