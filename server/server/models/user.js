8f2e300fa0a99b1d5984fb0accbe2100f962be13
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-02-25 00:04:56 +0300

    modify user models

--- server/server/models/user.js
+++ server/server/models/user.js
@@ -11,8 +11,8 @@ const userSchema = new mongoose.Schema({
 
   password: {
     type: String,
-    // required: true,
     select: false,
+    // required: true,
   },
 
   firstName: {
@@ -46,7 +46,7 @@ const userSchema = new mongoose.Schema({
     type: String,
   },
 
-  profilePicture: {
+  profilePictureURL: {
     type: String,
   },
 
@@ -95,66 +95,3 @@ userSchema.methods.generateAccessJWT = function () {
 };
 
 module.exports = mongoose.model("UserModel", userSchema);
-
-/*
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-
-  password: {
-    type: String,
-    required: true,
-  },
-
-  firstName: {
-    type: String,
-    required: true,
-  },
-
-  lastName: {
-    type: String,
-    required: true,
-  },
-
-  location: {
-    type: String,
-    required: true,
-  },
-
-  phone: {
-    type: Number,
-    required: true,
-    validate: {
-      validator: function (v) {
-        // This regex allows numbers, spaces, parentheses, and hyphens
-        return /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(v);
-      },
-      message: (props) => `${props.value} is not a valid phone number format`,
-    },
-  },
-
-  preferences: {
-    type: String,
-  },
-
-  profilePicture: {
-    type: String,
-  },
-
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-});
-
-module.exports = mongoose.model("User", userSchema);
-
-
-
-*/
