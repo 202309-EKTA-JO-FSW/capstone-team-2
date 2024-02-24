ea9f17683ac7c894edbbefb3e31a683ff4ae74af
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-02-25 00:06:42 +0300

    modify blackList  models

--- server/server/models/blackList.js
+++ server/server/models/blackList.js
@@ -12,21 +12,3 @@ const blackListSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("BlackListModel", blackListSchema);
-
-/*
-const mongoose = require("mongoose");
-
-const blackListSchema = new mongoose.Schema(
-  {
-    token: {
-      type: String,
-      required: true,
-      ref: "User",
-    },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("BlackList", blackListSchema);
-
- */
