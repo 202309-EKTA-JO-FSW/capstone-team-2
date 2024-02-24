db9a846563aef4b5b8d583cfa24a4625a3a0c1a1
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-02-24 21:55:44 +0300

    add Dependencies

--- server/server/routes/userRoute.js
+++ server/server/routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const { ensureAuth, ensureGuest } = require('../middleware/googleAuth');
+const { ensureAuth, ensureGuest } = require('../middlewares/googleAuth');
 
 // muna
 // const { createOrder } = require('../controller/orderController');
