b04e15190cef391bdc318d2061ee8341d45961f6
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-02-25 01:21:07 +0300

    modify user auth

--- server/server/middlewares/userAuth.js
+++ server/server/middlewares/userAuth.js
@@ -1,47 +1,64 @@
 /***********************************ramah */
-
-const UserModel = require("../models/user");
-const BlackListModel = require("../models/blackList");
+const userModel = require("../models/user");
+const blackListModel = require("../models/blackList");
 const jwt = require("jsonwebtoken");
 const cookie = require('cookie');
 
-const verifySignin = async (req, res, next) => {
-   try {
-     const authHeader = req.headers["cookie"];
+// For a user who's logged in
+const ensureAuth = async (req, res, next) => {
+  try {
+    if (req.isAuthenticated()) {
+      return next();
+    }; 
 
-     if(!authHeader){
-       return res.sendStatus(401);
-     };
-     const cookies = cookie.parse(authHeader);
-     const accessToken = cookies["SessionID"];  
+    const authHeader = req.headers["cookie"];
 
-     if (!accessToken) {
+    if(!authHeader){
       return res.sendStatus(401);
     };
     
-    const checkIfBlackListed = await BlackListModel.findOne({ token:  accessToken});
+    const cookies = cookie.parse(authHeader);
+    const accessToken = cookies["SessionID"];  
 
-    if (checkIfBlackListed) {
-      return res.status(401).json({ message: 'This session has expired. Please login'})
-    }
-
-    jwt.verify(accessToken, process.env.SECRET_ACCESS_TOKEN, async (err, decoded) => {
-       if (err) {
-         return res.status(401).json({ message: "This session has expired. Please login" });
-       };
-
-       const { id } = decoded;
-       const user = await UserModel.findById(id);
-       const { password, ...data } = user._doc;
-       req.user = data;
-       next();
-     });
-   } catch (err) {
-      res.status(500).json({ message: err.message})
+    if (!accessToken) {
+     return res.sendStatus(401);
    };
+   
+   const checkIfBlackListed = await blackListModel.findOne({ token:  accessToken});
+
+   if (checkIfBlackListed) {
+     return res.status(401).json({ message: 'This session has expired. Please login'})
+   }
+
+   jwt.verify(accessToken, process.env.SECRET_ACCESS_TOKEN, async (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "This session has expired. Please login" });
+      };
+
+      const { id } = decoded;
+      const user = await userModel.findById(id);
+      const { password, ...data } = user._doc;
+      req.user = data;
+      next();
+    });
+
+  } catch (err) {
+     res.status(500).json({ message: err.message})
+  };
 };
 
-const verifyIsAdmin = (req, res, next) => {
+// For a user who's not logged in
+const ensureGuest = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    res.redirect('/test');
+
+  } else {
+    return next();
+  };
+};
+
+// Check if the user is admin
+const verifyIsAdmin = async (req, res, next) => {
   try {
     const user = req.user;
     const { role } = user;
@@ -49,14 +66,17 @@ const verifyIsAdmin = (req, res, next) => {
     if (role === 'user') {
       return res.status(401).json({ message: "You are not authorized to view this page."});
     };
+
     next();
+
   } catch (err) {
     res.status(500).json({ message: err.message});
   };
 };
 
-module.exports = {verifySignin, 
-                  verifyIsAdmin};
+module.exports = { 
+  ensureAuth,
+  ensureGuest,
+  verifyIsAdmin
+};
 
-
-/********************************************* */
