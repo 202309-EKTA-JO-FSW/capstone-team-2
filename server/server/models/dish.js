64d72889ae52307e3b805830c7f3773ed56c10c6
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-02-25 00:28:01 +0300

    modify dish  models

--- server/server/models/dish.js
+++ server/server/models/dish.js
@@ -1,19 +1,3 @@
-/*
-const mongoose = require("mongoose");
-
-const dishSchema = new mongoose.Schema({
-  restaurantID: { type: mongoose.Schema.Types.ObjectId, required: true, ref : "restaurantSchema" },
-  dishName: { type: String, required: true },
-  dishImage: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  category: { type: [String], required: true },
-  review: { type: [String], required: true },
-});
-
-module.exports = mongoose.model("dishSchema", dishSchema);
-*/
-
 const mongoose = require("mongoose");
 
 const dishSchema = new mongoose.Schema({
@@ -47,9 +31,6 @@ const dishSchema = new mongoose.Schema({
     type: [String],
     required: true,
   },
-  // readyTime: {
-  //   type: Number,
-  // },
 });
 
 module.exports = mongoose.model("DishModel", dishSchema);
