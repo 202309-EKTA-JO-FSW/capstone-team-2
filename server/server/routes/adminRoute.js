99b2adacd121b89d08da7b457cf8d68393661558
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-02-26 00:37:12 +0300

    admin route clean code without sign

--- server/server/routes/adminRoute.js
+++ server/server/routes/adminRoute.js
@@ -1,67 +1,71 @@
 const express = require("express");
 const router = express.Router();
 const { route } = require("..");
-const { ensureAuth, verifyIsAdmin } = require('../middlewares/userAuth');
+const { ensureAuth, verifyIsAdmin } = require("../middlewares/userAuth");
 
 const adminController = require("../controllers/adminController");
 
-// farah
-
 // Define admin routes
+
 // for get all dishes
-router.get("/Dishes",ensureAuth, verifyIsAdmin, adminController.getDishItems);
+router.get("/Dishes", ensureAuth, verifyIsAdmin, adminController.getDishItems);
 // for add new Restaurant
-router.post("/Restaurant",ensureAuth, verifyIsAdmin, adminController.addRestaurant);
+router.post(
+  "/Restaurant",
+  ensureAuth,
+  verifyIsAdmin,
+  adminController.addRestaurant
+);
 // add dish for selected Restaurant
-router.post("/DishToRestaurant", ensureAuth, verifyIsAdmin,adminController.addDishToRestaurant); // Add this line
-
-// ahmad
-// router.post("/restaurant", adminController.addRestaurant);
-// router.post("/dish", adminController.addDish);
-
+router.post(
+  "/DishToRestaurant",
+  ensureAuth,
+  verifyIsAdmin,
+  adminController.addDishToRestaurant
+);
 // get all Restaurant
-router.get("/restaurants",ensureAuth, verifyIsAdmin, adminController.getAllRestaurants);
+router.get(
+  "/restaurants",
+  ensureAuth,
+  verifyIsAdmin,
+  adminController.getAllRestaurants
+);
 // get dishes for one Restaurant
-router.get("/restaurants/:restaurantId", ensureAuth, verifyIsAdmin,adminController.getAllDishes);
+router.get(
+  "/restaurants/:restaurantId",
+  ensureAuth,
+  verifyIsAdmin,
+  adminController.getAllDishes
+);
 // delete Restaurant or Restaurants
-router.delete("/restaurantDelete",ensureAuth, verifyIsAdmin, adminController.deleteRestaurant);
-
-// router.delete("/dishDelete", adminController.deleteDishs);
-
+router.delete(
+  "/restaurantDelete",
+  ensureAuth,
+  verifyIsAdmin,
+  adminController.deleteRestaurant
+);
 // delete dish for specific Restaurant
-router.delete("/dishDelete", ensureAuth, verifyIsAdmin,adminController.removeOneOrManyItems);
+router.delete(
+  "/dishDelete",
+  ensureAuth,
+  verifyIsAdmin,
+  adminController.removeOneOrManyItems
+);
 // for search dishes minPrice and maxPrice and category and name
-router.get("/searchDishes", ensureAuth, verifyIsAdmin,adminController.searchDishes);
-
-//edit restaurant 
-router.put("/restaurant/edit",ensureAuth, verifyIsAdmin, adminController.editRestaurant);
+router.get(
+  "/searchDishes",
+  ensureAuth,
+  verifyIsAdmin,
+  adminController.searchDishes
+);
+//edit restaurant
+router.put(
+  "/restaurant/edit",
+  ensureAuth,
+  verifyIsAdmin,
+  adminController.editRestaurant
+);
 // edit dish
-router.put("/dish/edit", ensureAuth, verifyIsAdmin,adminController.editDish);
+router.put("/dish/edit", ensureAuth, verifyIsAdmin, adminController.editDish);
 
 module.exports = router;
-
-/************************************************* */
-
-/*const express = require("express");
-const router = express.Router();
-const adminController = require("../controller/adminCtrl");
-
-// Define admin routes
-router.get('/Dishes',adminController.getDishItems);
-router.post("/Dishes", adminController.addDish);
-
-module.exports = router; 
-
-*/
-/************************************************ muna*/
-/*
-const express = require("express");
-const router = express.Router();
-const adminController = require("../controller/adminctrl");
-
-// Define admin routes
-router.get('/Dishes',adminController.getDishItems);
-router.post("/Dishes", adminController.addDish);
-
-module.exports = router; 
-*/
