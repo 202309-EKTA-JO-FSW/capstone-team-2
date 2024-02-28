d9ab1ca8b23931c31038844032405ecf6946f1a2
Mona <monabadee@gmail.com>
2024-02-28 23:13:54 +0300

    add router for past completed orders

--- server/server/routes/userRoute.js
+++ server/server/routes/userRoute.js
@@ -1,1 +1,17 @@
-// 
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const {
+  ensureGuest,
+  ensureAuth,
+  verifyIsAdmin,
+} = require("../middlewares/userAuth");
+
+// get past completed orders ok
+router.get(
+  "/orders/past",
+  //  isAuthenticated,
+  userController.getPastOrders
+);
+
+module.exports = router;
