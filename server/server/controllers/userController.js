106d2d9c0874b22aabdc383a72d39fad3ef0298e
Farah Alsoqi <Farahmanna97@gmail.com>
2024-02-29 23:27:16 +0300

    add function addToCart

--- server/server/controllers/userController.js
+++ server/server/controllers/userController.js
@@ -157,6 +157,79 @@ const getDishesByRestaurant = async (req, res) => {
   }
 };
 
+// Add Dish to Cart ok
+const addToCart = async (req, res) => {
+  try {
+    const { userID, dishID, quantity, specificRequests } = req.body;
+
+    // Validate the user
+    const user = await UserModel.findById(userID);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Find the dish by ID
+    const dish = await DishModel.findById(dishID);
+    if (!dish) {
+      return res.status(404).json({ message: "Dish not found" });
+    }
+
+    // Find or create the user's cart
+    let cart = await OrderModel.findOne({ userID, status: "waiting" });
+    if (!cart) {
+      cart = new OrderModel({
+        userID,
+        restaurant: dish.restaurantID,
+        totalBill: 0,
+        status: "waiting",
+        orderItems: [],
+      });
+    }
+
+    // Check if the dish is already in the cart
+    let existingItem = cart.orderItems.find((item) =>
+      item.dishID.equals(dish._id)
+    );
+    if (existingItem) {
+      // Update existing item in the cart
+      existingItem.quantity += quantity;
+      if (specificRequests) {
+        existingItem.specificRequests = specificRequests;
+      }
+    } else {
+      // Add a new item to the cart
+      cart.orderItems.push({
+        dishID: dish._id,
+        quantity,
+        specificRequests,
+      });
+    }
+
+    cart.totalBill = 0; // Reset totalBill to 0 before recalculating
+    for (const item of cart.orderItems) {
+      const dishItem = await DishModel.findById(item.dishID);
+      if (dishItem && dishItem.price) {
+        cart.totalBill += dishItem.price * item.quantity;
+      } else {
+        // Handle the case where dishItem or price is not available
+        throw new Error("Invalid dish or price");
+      }
+    }
+
+    // Save the cart
+    await cart.save();
+
+    res.status(201).json({
+      message: "Dish added to cart successfully",
+      cart: cart, // Send the full cart back to the user
+    });
+  } catch (error) {
+    console.error("Error adding dish to cart:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
 
 module.exports = {
   getPastOrders,
@@ -166,4 +239,5 @@ module.exports = {
   searchDishes,
   getDishById,
   getDishesByRestaurant,
+  addToCart,
 };
