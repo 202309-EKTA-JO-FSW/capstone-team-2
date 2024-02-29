e039929d36e810583b194e5bf14e4b1171811fc2
Farah Alsoqi <Farahmanna97@gmail.com>
2024-02-29 23:30:56 +0300

    sign in function

--- server/server/controllers/userController.js
+++ server/server/controllers/userController.js
@@ -229,6 +229,47 @@ const addToCart = async (req, res) => {
   }
 };
 
+// Sign in ok
+const signin = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(401).json({
+        message:
+          "Invalid email or password. Please try again with the correct credentials.",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      `${req.body.password}`,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message:
+          "Invalid email or password. Please try again with the correct credentials.",
+      });
+    }
+
+    let options = {
+      maxAge: 180 * 60 * 1000, // 3 hours
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+
+    const token = user.generateAccessJWT();
+    res.cookie("SessionID", token, options);
+
+    res.status(200).json({ message: "You have successfully logged in." });
+    res.end();
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 
 module.exports = {
@@ -240,4 +281,6 @@ module.exports = {
   getDishById,
   getDishesByRestaurant,
   addToCart,
+
+  signin,
 };
