b13663eaf26f506ead6faa5990af921ae1aab9c0
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-02-29 22:53:33 +0300

    add import func and route to get all dishes

--- server/server/routes/adminRoute.js
+++ server/server/routes/adminRoute.js
@@ -1,1 +1,14 @@
-// 
+const express = require("express");
+const router = express.Router();
+const { route } = require("..");
+const { ensureAuth, verifyIsAdmin } = require("../middlewares/userAuth");
+
+const adminController = require("../controllers/adminController");
+
+// Define admin routes
+
+// for get all dishes
+router.get("/Dishes", ensureAuth, verifyIsAdmin, adminController.getDishItems);
+
+
+module.exports = router;
