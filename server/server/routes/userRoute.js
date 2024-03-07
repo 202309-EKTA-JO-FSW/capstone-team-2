4009a63cbb770b8b134bd36228acb737b585ab1c
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-03-07 22:40:47 +0300

    reorder user routes

--- server/server/routes/userRoute.js
+++ server/server/routes/userRoute.js
@@ -7,6 +7,57 @@ const {
   verifyIsAdmin,
 } = require("../middlewares/userAuth");
 
+// detials for one dish for dynamic id in react ok
+router.get("/dishes/:itemId", userController.getDishById);
+
+//Get all Dishes in specific Restaurant ok
+router.get("/restaurants/:restaurantID", userController.getDishesByRestaurant);
+
+// for test
+router.get(
+  "/allusers",
+  // ensureAuth,
+  userController.getUsersData
+);
+
+router.get("/alltokens", userController.getTokens);
+
+router.delete("/delete", userController.removeUser);
+
+router.delete("/deletetokens", userController.removeTokens);
+
+// Check if the user not auth to login with google
+router.get("/google", ensureGuest, (_, res) => {
+  res.send('<a href="/auth/google">Authentication with Google</a>');
+});
+
+// signin & signout without google
+// sign in ok
+router.post("/signin", ensureGuest, userController.signin);
+
+// sign out ok
+router.get("/signout", userController.signout);
+
+// add new user ok
+router.post("/newuser", ensureGuest, userController.addNewUser);
+
+// update user info ok
+router.put(
+  "/updateinfo/:id",
+  // ensureAuth,
+  userController.updateUserData
+);
+
+// get all dishes for user ok
+router.get("/dishes", userController.getDishItems);
+
+// get one user profile ok
+router.get(
+  "/:id",
+  //  ensureAuth,
+  userController.getUserProfile
+);
+
 // get past completed orders ok
 router.get(
   "/orders/past",
@@ -29,58 +80,10 @@ router.patch(
   userController.cancelOrder
 );
 
-// get all dishes for user ok
-router.get("/dishes", userController.getDishItems);
-
 // search for dish name min max price and catogire ok
 router.get("/dishes/search", userController.searchDishes);
 
-// detials for one dish for dynamic id in react ok
-router.get("/dishes/:itemId", userController.getDishById);
-
-//Get all Dishes in specific Restaurant ok
-router.get("/restaurants/:restaurantID", userController.getDishesByRestaurant);
-
 // Add Dish to Cart ok
 router.post("/cart", userController.addToCart);
-
-// signin & signout without google
-// sign in ok
-router.post("/signin", ensureGuest, userController.signin);
-
-// sign out ok
-router.get("/signout", userController.signout);
-
-// get one user profile ok
-router.get(
-  "/:id",
-  //  ensureAuth,
-  userController.getUserProfile
-);
-
-// add new user ok
-router.post("/newuser", ensureGuest, userController.addNewUser);
-
-// update user info ok
-router.put(
-  "/updateinfo/:id",
-  // ensureAuth,
-  userController.updateUserData
-);
-
-// for test
-router.get(
-  "/allusers",
-  // ensureAuth,
-  userController.getUsersData
-);
-router.get("/alltokens", userController.getTokens);
-router.delete("/delete", userController.removeUser);
-router.delete("/deletetokens", userController.removeTokens);
-
-// Check if the user not auth to login with google
-router.get("/google", ensureGuest, (_, res) => {
-  res.send('<a href="/auth/google">Authentication with Google</a>');
-});
 
 module.exports = router;
