f05eca90906cade28f5792cbfc12ce671d3ce5eb
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-03-24 01:20:31 +0300

    modified user orofile and update profile clean

--- client/src/app/userprofile/updateInfo/page.js
+++ client/src/app/userprofile/updateInfo/page.js
@@ -1,10 +1,8 @@
-"use client"
-import { useAuth } from '@/app/context/userContext';
+"use client";
 import React, { useState, useEffect } from 'react';
 
-function UpdateProfileInfo({ params }) {
-  const {user} = useAuth()
-  // const [user, setUser] = useState(null);
+function UpdateProfileInfo() {
+  const [user, setUser] = useState(null);
   const [formData, setFormData] = useState({
     email: '',
     firstName: '',
@@ -13,36 +11,29 @@ function UpdateProfileInfo({ params }) {
     phone: '',
     profilePictureURL: ''
   });
+  const [successMessage, setSuccessMessage] = useState('');
+  const [error, setError] = useState('');
+
 
   useEffect(() => {
-    const fetchUser = async () => {
+    const storedUserInfo = localStorage.getItem('userInfo');
+    if (storedUserInfo) {
       try {
-        const res = await fetch(`http://localhost:3001/user/${user._id}`,{
-          method: "GET",
-          headers: { 
-          "Authorization": `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
-          "Content-Type": "application/json"}}
-        );
-        if (!res.ok) {
-          throw new Error('Failed to fetch user data');
-        }
-       const userData = await res.json();
-        setUser(userData);
+        const parsedUserInfo = JSON.parse(storedUserInfo);
+        setUser(parsedUserInfo);
+        setFormData(parsedUserInfo); // Set form data with fetched user data
       } catch (error) {
-        console.error('Error fetching user data:', error);
-
+        console.error('Error parsing user info JSON:', error);
       }
-    };
-   
-    fetchUser();
-  }, [params.id]);
+    }
+  }, []); // Fetch user profile only once on component mount
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    setError(''); // Reset error message on input change
   };
 
   const handleFileChange = (e) => {
-    // Assuming only one file is selected
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
@@ -53,67 +44,85 @@ function UpdateProfileInfo({ params }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(e)
     try {
+      const phoneRegex = /^7\d{8}$/;
+      if (!phoneRegex.test(formData.phone)) {
+        setError('Phone number must start with 7 and contain 9 digits.');
+        return;
+      }
+  
+    // Check if at least one field is empty
+     if (Object.values(formData).some(value => value === '')) {
+         setSuccessMessage('No changes have been made.');
+         return;
+       }
+  
+      // Filter out empty values from the form data
+      const formDataToUpdate = Object.fromEntries(
+        Object.entries(formData).filter(([key, value]) => value !== '')
+      );
+  
       const response = await fetch(`http://localhost:3001/user/updateinfo/${user._id}`, {
         method: 'PUT',
+        body: JSON.stringify(formDataToUpdate),
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData),
       });
+  
       if (!response.ok) {
         throw new Error('Failed to update user data');
       }
+      
       const updatedUser = await response.json();
-      setUser(updatedUser); // Update user state with updated data
+      localStorage.setItem('userInfo', JSON.stringify(updatedUser));
+      setUser(updatedUser);
+  
+      setSuccessMessage('Your data successfully updated!');
     } catch (error) {
       console.error(error);
+      setError('Failed to update user data');
     }
   };
-  if (!user) {
-    return <div>Loading...</div>;
-  }
+  
 
   return (
-    <div className='py-16'>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <div>
-          <label>Email:</label>
-          <input type="email" name="email" value={formData.email} onChange={(e) => handleChange(e)}  className='text-black'/>
+    <div className='bg-[#F9F9F7] py-24'>
+    <div className='text-black'>
+      <form onSubmit={(e) => handleSubmit(e)} className="max-w-md mx-auto mt-8 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
+          <input type="email" name="email" value={formData.email} onChange={(e) => handleChange(e)} className='text-black border border-gray-300 p-2 rounded-md w-full focus:border-gray-500'/>
         </div>
-        <div>
-          <label>First Name:</label>
-          <input type="text" name="firstName" value={formData.firstName} onChange={(e) => handleChange(e)} className='text-black'/>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">First Name:</label>
+          <input type="text" name="firstName" value={formData.firstName} onChange={(e) => handleChange(e)} className='text-black border border-gray-300 p-2 rounded-md w-full'/>
         </div>
-        <div>
-          <label>Last Name:</label>
-          <input type="text" name="lastName" value={formData.lastName} onChange={(e) => handleChange(e)} className='text-black'/>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Last Name:</label>
+          <input type="text" name="lastName" value={formData.lastName} onChange={(e) => handleChange(e)} className='text-black border border-gray-300 p-2 rounded-md w-full'/>
         </div>
-        <div>
-          <label>Location:</label>
-          <input type="text" name="location" value={formData.location} onChange={(e) => handleChange(e)} className='text-black'/>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Location:</label>
+          <input type="text" name="location" value={formData.location} onChange={(e) => handleChange(e)} className='text-black border border-gray-300 p-2 rounded-md w-full'/>
         </div>
-        <div>
-          <label>Phone:</label>
-          <input type="text" name="phone" value={formData.phone} onChange={(e) => handleChange(e)} className='text-black'/>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Phone:</label>
+          <input type="text" name="phone" value={formData.phone} onChange={(e) => handleChange(e)} className='text-black border border-gray-300 p-2 rounded-md w-full'/>
+          {error && (
+            <p className="text-red-500 text-sm">{error}</p>
+          )}
         </div>
-        <div>
-          <label>Profile Picture:</label>
-          <input type="file" accept="image/*" onChange={(e) => handleFileChange(e)} />
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Profile Picture:</label>
+          <input type="file" accept="image/*" onChange={(e) => handleFileChange(e)} className='text-black border border-gray-300 p-2 rounded-md w-full'/>
         </div>
-        <button type="submit" className='bg-white text-black'>Update</button>
+        <button type="submit" className='bg-[#AD343E] hover:bg-red-700 text-white py-2 px-4 rounded-md border border-gray-300'>Update</button>
       </form>
-
-      {/* Display user profile information */}
-      <div>
-        <h3>User Profile</h3>
-        <p>Email: {user.email}</p>
-        <p>Full Name: {user.firstName} {user.lastName}</p>
-        <p>Location: {user.location}</p>
-        <p>Phone: {user.phone}</p>
-        {formData.profilePictureURL && <img src={formData.profilePictureURL} alt="Profile" />}
-      </div>
+      {successMessage && (
+        <p className="text-green-500 mt-4 ml-[34rem] font-bold">{successMessage}</p>
+      )}
+    </div>
     </div>
   );
 }
--- client/src/app/userprofile/user/[id]/page.js
+++ client/src/app/userprofile/user/[id]/page.js
@@ -1,81 +1,7 @@
-// "use client"
-// import React, {useState ,useEffect } from 'react'
-
-// // async function getUser(id) {
-// // 	try {
-// //         const res = await fetch(`http://localhost:3001/user/${id}`);
-// //         if (!res.ok) {
-// //           throw new Error('Failed to fetch user data');
-// //         }
-// //         return res.json();
-// //       } catch (error) {
-// //         console.error('Error fetching user data:', error);
-// //         return null; // Return null or handle the error appropriately
-// //       }
-// // };
-
-
-// function UserInfo({ params }) {
-//   const [user, setUser] = useState(null);
-
-//   useEffect(() => {
-//     const fetchUser = async () => {
-//       try {
-//         const res = await fetch(`http://localhost:3001/user/${params.id}`);
-//         if (!res.ok) {
-//           throw new Error('Failed to fetch user data');
-//         }
-//         const userData = await res.json();
-//         setUser(userData);
-//       } catch (error) {
-//         console.error('Error fetching user data:', error);
-
-//       }
-//     };
-   
-//     fetchUser();
-//   }, [params.id]);
-
-//   if (!user) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div className="bg-white overflow-hidden shadow rounded-lg border">
-//       <div className="px-4 py-5 sm:px-6">
-//         <img src='https://img.freepik.com/free-photo/top-view-meals-tasty-yummy-different-pastries-dishes-brown-surface_140725-14554.jpg?w=740&t=st=1710539310~exp=1710539910~hmac=dec1def9f8a2d99c4aa1d57bbe73bee4f72087b7522d0035cf3e76d74bc813af' className='w-12 rounded-[100%]' alt="User avatar" />
-//         <h3 className="text-lg leading-6 font-medium text-gray-900">User</h3>
-//         <p className="mt-1 max-w-2xl text-sm text-gray-500">This is some information about the user.</p>
-//       </div>
-//       <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-//         <dl className="sm:divide-y sm:divide-gray-200">
-//           <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-//             <dt className="text-sm font-medium text-gray-500">Full name</dt>
-//             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{user.firstName} {user.lastName}</dd>
-//           </div>
-//           <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-//             <dt className="text-sm font-medium text-gray-500">Email address</dt>
-//             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{user.email}</dd>
-//           </div>
-//           <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-//             <dt className="text-sm font-medium text-gray-500">Phone number</dt>
-//             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{user.phone}</dd>
-//           </div>
-//           <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-//             <dt className="text-sm font-medium text-gray-500">Address</dt>
-//             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{user.location}</dd>
-//           </div>
-//         </dl>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default UserInfo;
-
 "use client"
 import { useAuth } from '@/app/context/userContext';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React, {useState ,useEffect } from 'react'
 
 // async function getUser(id) {
@@ -94,6 +20,7 @@ import React, {useState ,useEffect } from 'react'
 
 function UserInfo({ params }) {
   const [user, setUser] = useState(null);
+  const router = useRouter()
   // const { token } = useAuth();
   useEffect(() => {
     const fetchUser = async () => {
@@ -124,9 +51,9 @@ function UserInfo({ params }) {
   }
 
   return (
-    <div className="bg-white overflow-hidden shadow rounded-lg border">
+    <div className="bg-white overflow-hidden shadow rounded-lg border mt-14">
       <div className="px-4 py-5 sm:px-6">
-        <img src='https://img.freepik.com/free-photo/top-view-meals-tasty-yummy-different-pastries-dishes-brown-surface_140725-14554.jpg?w=740&t=st=1710539310~exp=1710539910~hmac=dec1def9f8a2d99c4aa1d57bbe73bee4f72087b7522d0035cf3e76d74bc813af' className='w-12 rounded-[100%]' alt="User avatar" />
+        <img src={user.profilePictureURL || 'https://i.pinimg.com/564x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg'} className='w-12 rounded-[100%]' alt="User avatar" />
         <h3 className="text-lg leading-6 font-medium text-gray-900">User</h3>
         <p className="mt-1 max-w-2xl text-sm text-gray-500">This is some information about the user.</p>
       </div>
@@ -149,7 +76,8 @@ function UserInfo({ params }) {
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{user.location}</dd>
           </div>
         </dl>
-        <Link href={"/userprofile/updateInfo"}>Edit</Link>
+        <button className='text-white mt-10 ml-8 mb-5 border bg-[#AD343E] py-2 px-4 rounded-md font-bold'><Link href={`http://localhost:3000/userprofile/updateInfo`}>Update Your Info</Link></button>
+
       </div>
     </div>
   );
