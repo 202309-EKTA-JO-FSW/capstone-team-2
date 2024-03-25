86652d24f7474455332c2b7561cd77eb6318eda1
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-03-25 05:20:59 +0300

    context  clean code

--- client/src/app/context/userContext.js
+++ client/src/app/context/userContext.js
@@ -1,27 +1,5 @@
-// "use client"
-// import React, { createContext, useState, useContext, useEffect } from 'react';
-
-// const AuthContext = createContext();
-
-// export const AuthProvider = ({ children }) => {
-//   const [user, setUser] = useState();
-
-//   useEffect(() => {
-//     let userData = localStorage.getItem('userInfo');
-//      setUser(userData ? JSON.parse(userData) : null);
-// },[])
-
-//   return (
-//     <AuthContext.Provider value={{ user, setUser}}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export const useAuth = () => useContext(AuthContext);
-
-"use client"
-import React, { createContext, useState, useContext, useEffect } from 'react';
+"use client";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const AuthContext = createContext();
 
@@ -30,15 +8,15 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState();
 
   useEffect(() => {
-    let userData = localStorage.getItem('userInfo');
-     setUser(userData ? JSON.parse(userData) : null);
+    let userData = localStorage.getItem("userInfo");
+    setUser(userData ? JSON.parse(userData) : null);
 
-     let token = localStorage.getItem('token');
-     setToken(token ? JSON.parse(token) : null);
-},[])
+    let token = localStorage.getItem("token");
+    setToken(token ? JSON.parse(token) : null);
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser, token}}>
+    <AuthContext.Provider value={{ user, setUser, token }}>
       {children}
     </AuthContext.Provider>
   );
