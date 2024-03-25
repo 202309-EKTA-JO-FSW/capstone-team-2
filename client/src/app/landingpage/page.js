f79eb646517c499a41929b05b6aa9a4eea10875f
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-03-25 05:24:57 +0300

    landing page clean code

--- client/src/app/landingpage/page.js
+++ client/src/app/landingpage/page.js
@@ -1,44 +1,8 @@
-// "use client";
-// import React, { useState, useEffect } from "react";
 import React from "react";
 import Hero from "../components/Hero";
-// import { Container } from "react-bootstrap";
-// import HomePageItems from "../../components/HomePageItems/HomePageItems";
-// import "./HomePage.css";
-// import db from "../../firebase";
-// import Hero from "../../components/Hero/Hero";
-// import SearchBar from "../../components/SearchBar/SearchBar";
-
-
-// After deciding on the database structure,the example function below should be deleted and
-// replaced with a function that reads categories from firebase, takes 8 items by random
-// and passes it to HomePageItems as a prop
 
 function HomePage() {
-  // const [dbItems, setDbItems] = useState([]);
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const response = await db.collection("homePageItems").get();
-  //       const itemList = [];
-  //       response.forEach((doc) => itemList.push(doc.data()));
-  //       setDbItems(itemList);
-  //     } catch (err) {
-  //       alert(err);
-  //     }
-  //   }
-  //   fetchData();
-  // }, []);
-
-  return (
-    // <Container>
-    //   <Hero />
-    //   <HomePageItems dbItems={dbItems} />
-    //   <SearchBar />
-    // </Container>
-    <Hero/>
-  );
+  return <Hero />;
 }
 
 export default HomePage;
